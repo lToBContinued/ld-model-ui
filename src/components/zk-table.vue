63fb9a67bc4bf312e73881863e89abf7c3fe5e7c
lToBContinued <wt3212834434@outlook.com>
2025-08-01 11:04:01 +0800

    基本完成评估结果页面

--- src/components/zk-table.vue
+++ src/components/zk-table.vue
@@ -26,7 +26,7 @@
       </template>
     </el-table>
     <zk-pagination
-      :total="100"
+      :total="total"
       v-model:current-page="pageConfig.currentPage"
       v-model:page-size="pageConfig.pageSize"
       @change="pageChange"
@@ -97,6 +97,10 @@ defineProps({
       currentPage: 1,
       pageSize: 10,
     }),
+  },
+  total: {
+    type: Number,
+    default: 0,
   },
 })
 
