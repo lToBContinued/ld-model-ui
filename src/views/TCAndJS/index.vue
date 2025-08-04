6afd2289e49444ecda7488201a9adf433197c021
lToBContinued <wt3212834434@outlook.com>
2025-08-04 10:50:02 +0800

    修改多选框组件

--- src/views/TCAndJS/index.vue
+++ src/views/TCAndJS/index.vue
@@ -1,29 +1,18 @@
 <template>
   <div class="TCAndJS">
     <module-title></module-title>
-    <zk-checkbox v-model="checkboxValue" :options="checkboxOptions" border></zk-checkbox>
+    <zk-dialog :model-value="isShow" @close="closeDialog"></zk-dialog>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref, watchEffect } from 'vue'
+import { ref } from 'vue'
 
-const checkboxValue = ref([])
-const checkboxOptions = ref([
-  {
-    label: '典型目标TC',
-    value: '0',
-    disabled: true,
-  },
-  {
-    label: '区域JS',
-    value: '1',
-  },
-])
+const isShow = ref(true)
 
-watchEffect(() => {
-  console.log('>>>>> file: index.vue ~ method:  <<<<<\n', checkboxValue.value) // TODO: 删除
-})
+const closeDialog = () => {
+  isShow.value = false
+}
 </script>
 
 <style scoped lang="scss"></style>
