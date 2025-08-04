5412172784dcc19c12b7bccec419e373be05abd8
lToBContinued <wt3212834434@outlook.com>
2025-08-04 10:37:07 +0800

    修改多选框组件

--- src/views/TCAndJS/index.vue
+++ src/views/TCAndJS/index.vue
@@ -1,10 +1,29 @@
 <template>
   <div class="TCAndJS">
     <module-title></module-title>
-    典型目标TC与区域JS
+    <zk-checkbox v-model="checkboxValue" :options="checkboxOptions" border></zk-checkbox>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, watchEffect } from 'vue'
+
+const checkboxValue = ref([])
+const checkboxOptions = ref([
+  {
+    label: '典型目标TC',
+    value: '0',
+    disabled: true,
+  },
+  {
+    label: '区域JS',
+    value: '1',
+  },
+])
+
+watchEffect(() => {
+  console.log('>>>>> file: index.vue ~ method:  <<<<<\n', checkboxValue.value) // TODO: 删除
+})
+</script>
 
 <style scoped lang="scss"></style>
