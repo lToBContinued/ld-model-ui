4abe61d538cb5458919c1a385dbce7706dd28d7f
lToBContinued <wt3212834434@outlook.com>
2025-08-05 09:36:38 +0800

    修改理论研究成果页面

---
+++ src/views/theoryKnowledge/configs/tableConfigs.ts
@@ -0,0 +1,15 @@
+export const resultTableColumns = [
+  {
+    prop: 'project',
+    label: '项目名称',
+  },
+  {
+    prop: 'score',
+    label: '得分',
+  },
+  {
+    prop: 'date',
+    label: '等级',
+    slot: 'level',
+  },
+]
