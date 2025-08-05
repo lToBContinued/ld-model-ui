1eacf1f067d727eba7c0695089493263a64b0ab4
lToBContinued <wt3212834434@outlook.com>
2025-08-05 13:38:33 +0800

    删除测试代码

--- src/views/TCAndJS/index.vue
+++ src/views/TCAndJS/index.vue
@@ -1,18 +1,9 @@
 <template>
   <div class="TCAndJS">
     <module-title></module-title>
-    <zk-dialog :model-value="isShow" @close="closeDialog"></zk-dialog>
   </div>
 </template>
 
-<script setup lang="ts">
-import { ref } from 'vue'
-
-const isShow = ref(true)
-
-const closeDialog = () => {
-  isShow.value = false
-}
-</script>
+<script setup lang="ts"></script>
 
 <style scoped lang="scss"></style>
