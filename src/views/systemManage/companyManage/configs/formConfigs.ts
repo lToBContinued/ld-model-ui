e35587ce0baced5a0b748b325c144970b8545a91
lToBContinued <wt3212834434@outlook.com>
2025-08-05 14:47:53 +0800

    搭建评估单位管理页面

---
+++ src/views/systemManage/companyManage/configs/formConfigs.ts
@@ -0,0 +1,18 @@
+export const addCompanyFormConfig = [
+  {
+    label: '评估单位',
+    prop: 'company',
+  },
+  {
+    label: '单位编号',
+    prop: 'companyNumber',
+  },
+  {
+    label: '部门',
+    prop: 'department',
+  },
+  {
+    label: '代号',
+    prop: 'code',
+  },
+]
