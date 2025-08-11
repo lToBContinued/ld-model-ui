7ed8bf66b642175d06b600e50c90637642790c69
lToBContinued <wt3212834434@outlook.com>
2025-08-11 15:08:54 +0800

    封装drawer组件

--- src/views/theoryKnowledge/index.vue
+++ src/views/theoryKnowledge/index.vue
@@ -1,9 +1,17 @@
 <template>
   <div class="container">
-    <module-title title="人才评估系统"></module-title>
+    <zk-drawer v-model="drawer" title="理论知识">
+      <template #footer>
+        <zk-button>取消</zk-button>
+        <zk-button type="primary">确定</zk-button>
+      </template>
+    </zk-drawer>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+const drawer = ref(true)
+</script>
 
 <style scoped lang="scss"></style>
