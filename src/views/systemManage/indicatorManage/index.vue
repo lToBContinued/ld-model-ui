f1fed3ff1f06a2784201f695bd61121f4cf77e74
lToBContinued <wt3212834434@outlook.com>
2025-08-15 11:00:48 +0800

    m

--- src/views/systemManage/indicatorManage/index.vue
+++ src/views/systemManage/indicatorManage/index.vue
@@ -2,12 +2,12 @@
   <div class="indicatorManage">
     <zk-card>
       <el-row>
-        <el-col :span="6">
+        <el-col :span="8">
           <div class="tree">
-            <zk-tree></zk-tree>
+            <zk-tree :data="treeData" :active="{ append: true, edit: true, remove: true, check: true }"></zk-tree>
           </div>
         </el-col>
-        <el-col :span="18">
+        <el-col :span="16">
           <div class="other"></div>
         </el-col>
       </el-row>
@@ -15,7 +15,22 @@
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { reactive } from 'vue'
+
+const treeData = reactive([
+  {
+    id: 1,
+    label: 'ld指标',
+    children: [
+      {
+        id: 2,
+        label: '一级指标',
+      },
+    ],
+  },
+])
+</script>
 
 <style scoped lang="scss">
 .tree {
@@ -23,9 +38,14 @@
   border: 1px solid red;
   height: 200px;
 }
+
 .other {
   width: 100%;
   border: 1px solid red;
   height: 200px;
+}
+
+::v-deep(.el-card__body) {
+  height: 100vh;
 }
 </style>
