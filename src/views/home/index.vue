27d65e9b508de5a4a6c0c38cfa386c11baa89aa1
lToBContinued <wt3212834434@outlook.com>
2025-08-25 10:34:04 +0800

    封装级联选择器

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,7 +1,48 @@
 <template>
-  <div></div>
+  <div>
+    <zk-cascader v-model="data" :options="options" style="width: 400px"></zk-cascader>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, watchEffect } from 'vue'
+const data = ref()
+const options = ref([
+  {
+    value: 1,
+    label: '选项1',
+    children: [
+      {
+        value: 11,
+        label: '选项1-1',
+        children: [
+          {
+            value: 111,
+            label: '选项1-1-1',
+          },
+        ],
+      },
+      {
+        value: 12,
+        label: '选项1-2',
+      },
+    ],
+  },
+  {
+    value: 2,
+    label: '选项2',
+    children: [
+      {
+        value: 21,
+        label: '选项2-1',
+      },
+    ],
+  },
+])
+
+watchEffect(() => {
+  console.log('>>>>> file: index.vue ~ method: data <<<<<\n', data.value) // TODO: 删除
+})
+</script>
 
 <style scoped lang="scss"></style>
