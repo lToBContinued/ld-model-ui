1090b1dc12141fe7e83981a943ae489450d0c302
lToBContinued <wt3212834434@outlook.com>
2025-08-26 15:48:40 +0800

    封装表单生成组件

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,48 +1,17 @@
 <template>
   <div>
-    <zk-cascader v-model="data" :options="options" style="width: 400px"></zk-cascader>
+    <form-configurator v-model="labelFormConfig" :indicator-id="111"></form-configurator>
+    <zk-button type="primary" @click="submit">提交</zk-button>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref, watchEffect } from 'vue'
-const data = ref()
-const options = ref([
-  {
-    value: 1,
-    label: '选项1',
-    children: [
-      {
-        value: 11,
-        label: '选项1-1',
-        children: [
-          {
-            value: 111,
-            label: '选项1-1-1',
-          },
-        ],
-      },
-      {
-        value: 12,
-        label: '选项1-2',
-      },
-    ],
-  },
-  {
-    value: 2,
-    label: '选项2',
-    children: [
-      {
-        value: 21,
-        label: '选项2-1',
-      },
-    ],
-  },
-])
+import { ref } from 'vue'
+const labelFormConfig = ref('')
 
-watchEffect(() => {
-  console.log('>>>>> file: index.vue ~ method: data <<<<<\n', data.value) // TODO: 删除
-})
+const submit = () => {
+  console.log('>>>>> file: index.vue ~ method: submit <<<<<\n', labelFormConfig.value) // TODO: 删除
+}
 </script>
 
 <style scoped lang="scss"></style>
